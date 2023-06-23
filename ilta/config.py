1856d4cd3b430f2c0fcd1b9d90644029b88a3b99
am <am@host.com>
2023-06-23 12:32:03 +0430

    Use literal for typing in config

--- ilta/config.py
+++ ilta/config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Literal
 
 @dataclass
 class MainConfig:
@@ -6,4 +7,6 @@ class MainConfig:
     num_channels: int = 15
     ts_duration: float = 0.03
     channel_per_ts: int = 3
-    tx_strategy: str = 'count' # Values: count, shuffle, random
+    tx_strategy: Literal['count', 'shuffle', 'random'] = 'count'
+    listen_duration: float = 15
+    rx_strategy: Literal['count', 'shuffle', 'random'] = 'count'
